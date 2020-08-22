3061f1b1ae7027c02d559cd0b8b7ac946414f942
acastemoreno <acastemoreno@gmail.com>
2020-08-22 18:28:15 -0500

    all

--- demo/lib/demo_web/router.ex
+++ demo/lib/demo_web/router.ex
@@ -2,21 +2,22 @@ defmodule DemoWeb.Router do
   use DemoWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", DemoWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    resources("/users", UserController)
   end
 
   # Other scopes may use custom stacks.
@@ -35,8 +36,8 @@ defmodule DemoWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: DemoWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: DemoWeb.Telemetry)
     end
   end
 end
